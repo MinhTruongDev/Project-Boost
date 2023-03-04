96c278ec15131aaea4005d8031202f13ef2790b8
Phan Minh Trường <104303855+MinhTruongDev@users.noreply.github.com>
2023-03-04 15:52:47 +0700

    Add Debug key and remake Level 1

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -9,6 +9,7 @@ public class CollisionHandler : MonoBehaviour
     //PARAMETERS
     [SerializeField] float delayTime = 1f;
     [SerializeField] AudioClip[] _audioClips;
+    [SerializeField] ParticleSystem[] _particleSystems;
 
     //CACHE - references for readability or speed
     AudioSource _audioSource;
@@ -16,27 +17,45 @@ public class CollisionHandler : MonoBehaviour
     //STATE - private instance (member) variables
     private int currentSceneIndex;
     private bool isTransitioning = false;
+    private bool collisionDisabled = false;
+
     void Start()
     {
         _audioSource = GetComponent<AudioSource>();
     }
+    void Update()
+    {
+        RespondToDebugKey();
+    }
+
+    private void RespondToDebugKey()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            LoadNextLevel();
+        }
+        else if (Input.GetKeyDown(KeyCode.C))
+        {
+            collisionDisabled = !collisionDisabled;
+        }
+    }
+
     void OnCollisionEnter(Collision other)
     {
-        if (!isTransitioning)
+        if (isTransitioning || collisionDisabled) { return; }
+
+        switch (other.gameObject.tag)
         {
-            switch (other.gameObject.tag)
-            {
-                case "Success":
-                    InvokeMethod("LoadNextLevel", delayTime, other.gameObject.tag);
-                    break;
-                case "Friendly":
-                    Debug.Log("Friendly");
-                    break;
-                default:
-                    InvokeMethod("ReloadScene", delayTime, other.gameObject.tag);
-                    break;
-            }
+            case "Success":
+                InvokeMethod("LoadNextLevel", delayTime, other.gameObject.tag);
+                break;
+            case "Friendly":
+                break;
+            default:
+                InvokeMethod("ReloadScene", delayTime, other.gameObject.tag);
+                break;
         }
+
     }
 
     private void PlayAudio(string objectTag)
@@ -47,7 +66,7 @@ public class CollisionHandler : MonoBehaviour
                 _audioSource.clip = _audioClips[0];
                 _audioSource.Play();
                 break;
-            default:
+            default: //Death
                 _audioSource.clip = _audioClips[1];
                 _audioSource.Play();
                 break;
@@ -57,10 +76,26 @@ public class CollisionHandler : MonoBehaviour
     void InvokeMethod(string methodName, float delayTime, string objectTag)
     {
         isTransitioning = true;
-        PlayAudio(objectTag);        
+        PlayAudio(objectTag);
+        PlayParticleSystem(objectTag);
         GetComponent<Movement>().enabled = false;
         Invoke(methodName, delayTime);
     }
+
+    private void PlayParticleSystem(string objectTag)
+    {
+
+        switch (objectTag)
+        {
+            case "Success":
+                _particleSystems[0].Play();
+                break;
+            default: //Death
+                _particleSystems[1].Play();
+                break;
+        }
+    }
+
     void ReloadScene()
     {
         currentSceneIndex = SceneManager.GetActiveScene().buildIndex;

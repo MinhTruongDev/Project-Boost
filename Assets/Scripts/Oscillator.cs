5e7566a0f4a47ddbffd9b41fe6dfea0ff71dd124
Phan Minh Trường <104303855+MinhTruongDev@users.noreply.github.com>
2023-03-12 19:39:44 +0700

    Add Level 3 and fix NaN bug

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Oscillator : MonoBehaviour
 {
     //PARAMETERS
-    [SerializeField] Vector3 movementVector;    
+    [SerializeField] Vector3 movementVector;
     [SerializeField] float period = 2f;
     Vector3 startingPosition;
     float movementFactor;
@@ -24,11 +24,15 @@ public class Oscillator : MonoBehaviour
     }
     void Update()
     {
-        cycles = Time.time / period;  // return continually growing value overtime || decide how much the wall oscillating
+        if (period <= Mathf.Epsilon) { return; }
+        cycles = Time.time / period; // return continually growing value overtime || decide how much the wall oscillating
         float rawSinWave = Mathf.Sin(cycles * tau); //return value (-1,1)
         movementFactor = (rawSinWave + 1f) / 2f; // to make rawSinWave return value (0,1) for movementFactor
         Vector3 offset = movementVector * movementFactor;
         transform.position = startingPosition + offset;
+
+
+
     }
     //PRIVATE METHOD
 }
